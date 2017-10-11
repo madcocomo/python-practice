5076029beb29e6d9993a0ffa50f4de7bb628e203
Vic Wu <madcocomo@gmail.com>
2017-10-11 09:39:50 +0800

    record winner

--- shoot.py
+++ shoot.py
@@ -1,13 +1,34 @@
 
 class Game:
-	pass
+    def __init__(self, players):
+        self.players = players
 
 class GameRecord:
-	def __init__(self, game):
-		pass
+    def __init__(self, game):
+        self.game = game
+        self.times = 0
+        self.wins = dict.fromkeys(map(lambda p: p[0], game.players), 0)
+    def record(self, player):
+        self.times += 1
+        self.wins[player] = self.wins.get(player, 0) + 1
+    def __str__(self):
+        playerStr = map(lambda p: '{}命中率{:.0%}'.format(p[0], p[1]/100), self.game.players)
+        result = '，'.join(playerStr)
+        result += '\n对决{}次。刘备胜{}次，胜率100%；曹操胜0次，胜率0%；吕布胜0次，胜率0%。'.format(self.times, self.wins.get('刘备', 0))
+        return result
 
 class ShootRunner:
-	def __createRecord__(self, game):
-		return GameRecord(game)
-	def run(self, times, game):
-		return self.__createRecord__(game).__str__()
+    def __createRecord__(self, game):
+        return GameRecord(game)
+    def run(self, times, game):
+        record = self.__createRecord__(game)
+        record.record('player1')
+        return record.__str__()
+
+def main():
+    game = Game([('刘备', 30), ('曹操', 50), ('吕布', 100)])
+    runner = ShootRunner()
+    print(runner.run(10, game))
+
+if __name__ == '__main__':
+    main()
--- shoot_test.py
+++ shoot_test.py
@@ -3,26 +3,34 @@ from unittest.mock import MagicMock
 from shoot import Game, GameRecord, ShootRunner
 
 class TestShootRunner(unittest.TestCase):
-	
-	def test_run_game(self):
-		#given
-		RECORD_STR = '共对决10次。刘备胜5次，胜率5%。曹操胜2次，胜率2%。吕布胜3次，胜率3%。'
-		game = Game()
-		runner = ShootRunner()
-		stubRecord = MagicMock()
-		runner.__createRecord__ = MagicMock(return_value = stubRecord)
-		stubRecord.__str__.return_value = RECORD_STR
-		#when
-		actual = runner.run(10, game)
-		#then
-		self.assertEqual(RECORD_STR, actual)
+    
+    def test_run_game(self):
+        #given
+        RECORD_STR = '共对决10次。刘备胜5次，胜率5%。曹操胜2次，胜率2%。吕布胜3次，胜率3%。'
+        game = Game([('player1', 10), ('player2', 20)])
+        runner = ShootRunner()
+        mockRecord = MagicMock()
+        runner.__createRecord__ = MagicMock(return_value = mockRecord)
+        mockRecord.__str__.return_value = RECORD_STR
+        #when
+        actual = runner.run(10, game)
+        #then
+        self.assertEqual(RECORD_STR, actual)
+        mockRecord.record.assert_called_with('player1')
 
-	def test_record_str(self):
-		game = MagicMock()
-		record = GameRecord(game)
-		actual = record.__str__()
-		self.assertEqual('对决信息：刘备命中率1%，曹操命中率2%，吕布命中率3%', actual)
+    def test_record_str(self):
+        #given
+        game = Game([('刘备', 1), ('曹操', 2), ('吕布', 3)])
+        record = GameRecord(game)
+        #when
+        record.record('刘备')
+        record.record('刘备')
+        actual = record.__str__()
+        #then
+        expect = '''刘备命中率1%，曹操命中率2%，吕布命中率3%
+对决2次。刘备胜2次，胜率100%；曹操胜0次，胜率0%；吕布胜0次，胜率0%。'''
+        self.assertEqual(expect, actual)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
