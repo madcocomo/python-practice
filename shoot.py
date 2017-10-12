a0fc3e4157a9e0bafe556b19ebecaf070c95590d
Vic Wu <madcocomo@gmail.com>
2017-10-12 08:52:10 +0800

    out put game records

--- shoot.py
+++ shoot.py
@@ -10,19 +10,22 @@ class GameRecord:
         self.wins = dict.fromkeys(map(lambda p: p[0], game.players), 0)
     def record(self, player):
         self.times += 1
-        self.wins[player] = self.wins.get(player, 0) + 1
-    def __str__(self):
+        self.wins[player] += 1
+    def playersInfo(self):
         playerStr = map(lambda p: '{}命中率{:.0%}'.format(p[0], p[1]/100), self.game.players)
-        result = '，'.join(playerStr)
-        result += '\n对决{}次。刘备胜{}次，胜率100%；曹操胜0次，胜率0%；吕布胜0次，胜率0%。'.format(self.times, self.wins.get('刘备', 0))
-        return result
+        return '，'.join(playerStr)
+    def gameInfo(self):
+        gameStr = map(lambda winner: '{}胜{}次，胜率{:.0%}'.format(winner[0], winner[1], winner[1]/self.times), self.wins.items())
+        return '对决{}次。'.format(self.times) + '；'.join(gameStr) + "。"
+    def __str__(self):
+        return self.playersInfo() + '\n' + self.gameInfo()
 
 class ShootRunner:
     def __createRecord__(self, game):
         return GameRecord(game)
     def run(self, times, game):
         record = self.__createRecord__(game)
-        record.record('player1')
+        record.record(game.players[0][0])
         return record.__str__()
 
 def main():
